70338b385636bea8fc782bfd1fe01d7e046eb984
Matt Friedman <maf675@gmail.com>
2014-06-10 10:55:36 -0700

    Remove built-in title, close button transition fx, and other fixes

--- language/en/boardannouncements_common.php
+++ language/en/boardannouncements_common.php
@@ -39,6 +39,5 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'BOARD_ANNOUNCEMENT'			=> 'Board Announcement',
 	'BOARD_ANNOUNCEMENT_CLOSE'		=> 'Close announcement',
 ));
