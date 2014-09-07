258a545ddb438ac640c35ab69a665d4f456dd285
Matt Friedman <maf675@gmail.com>
2014-09-07 01:22:38 -0700

    Load language file only when needed

--- tests/event/listener_test.php
+++ tests/event/listener_test.php
@@ -117,74 +117,8 @@ class event_listener_test extends \phpbb_database_test_case
 	public function test_getSubscribedEvents()
 	{
 		$this->assertEquals(array(
-			'core.user_setup',
 			'core.page_header_after',
 		), array_keys(\phpbb\boardannouncements\event\listener::getSubscribedEvents()));
-	}
-
-	/**
-	* Data set for test_load_language_on_setup
-	*
-	* @return array Array of test data
-	* @access public
-	*/
-	public function load_language_on_setup_data()
-	{
-		return array(
-			array(
-				array(),
-				array(
-					array(
-						'ext_name' => 'phpbb/boardannouncements',
-						'lang_set' => 'boardannouncements_common',
-					),
-				),
-			),
-			array(
-				array(
-					array(
-						'ext_name' => 'foo/bar',
-						'lang_set' => 'foobar',
-					),
-				),
-				array(
-					array(
-						'ext_name' => 'foo/bar',
-						'lang_set' => 'foobar',
-					),
-					array(
-						'ext_name' => 'phpbb/boardannouncements',
-						'lang_set' => 'boardannouncements_common',
-					),
-				),
-			),
-		);
-	}
-
-	/**
-	* Test the load_language_on_setup event
-	*
-	* @dataProvider load_language_on_setup_data
-	* @access public
-	*/
-	public function test_load_language_on_setup($lang_set_ext, $expected_contains)
-	{
-		$this->set_listener();
-
-		$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
-		$dispatcher->addListener('core.user_setup', array($this->listener, 'load_language_on_setup'));
-
-		$event_data = array('lang_set_ext');
-		$event = new \phpbb\event\data(compact($event_data));
-		$dispatcher->dispatch('core.user_setup', $event);
-
-		$lang_set_ext = $event->get_data_filtered($event_data);
-		$lang_set_ext = $lang_set_ext['lang_set_ext'];
-
-		foreach ($expected_contains as $expected)
-		{
-			$this->assertContains($expected, $lang_set_ext);
-		}
 	}
 
 	/**
