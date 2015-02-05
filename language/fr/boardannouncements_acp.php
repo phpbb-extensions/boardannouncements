6109d425b2e85df24a45c60a2950c8d69e1e38b3
Raphaël <Galixte@users.noreply.github.com>
2015-02-05 03:57:58 +0100

    Update for FRENCH translation for boardannouncements_acp.php file
    Update for FRENCH translation for boardannouncements_acp.php file

--- language/fr/boardannouncements_acp.php
+++ language/fr/boardannouncements_acp.php
@@ -2,7 +2,7 @@
 /**
 *
 * Board Announcements extension for the phpBB Forum Software package.
-* French translation by ForumsFaciles (www.forumsfaciles.fr)
+* French translation by ForumsFaciles (www.forumsfaciles.fr) & Galixte (http://www.galixte.com)
 *
 * @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
@@ -39,15 +39,15 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'BOARD_ANNOUNCEMENTS_SETTINGS'			=> 'Paramètres de “ Annonces du forum ”',
+	'BOARD_ANNOUNCEMENTS_SETTINGS'			=> 'Paramètres de l’annonce du forum',
 	'BOARD_ANNOUNCEMENTS_SETTINGS_EXPLAIN'	=> 'Sur cette page vous pouvez gérer et créer une annonce de forum qui sera affichée sur chaque page de votre forum.',
 
-	'BOARD_ANNOUNCEMENTS_ENABLE'			=> 'Afficher cette annonce de forum',
-	'BOARD_ANNOUNCEMENTS_GUESTS'			=> 'Permettre aux invités de voir cette annonce',
-	'BOARD_ANNOUNCEMENTS_DISMISS'			=> 'Permettre aux utilisateurs de masquer cette annonce',
+	'BOARD_ANNOUNCEMENTS_ENABLE'			=> 'Afficher l’annonce du forum',
+	'BOARD_ANNOUNCEMENTS_GUESTS'			=> 'Permettre aux invités de voir l’annonce du forum',
+	'BOARD_ANNOUNCEMENTS_DISMISS'			=> 'Permettre aux utilisateurs de masquer l’annonce du forum',
 
 	'BOARD_ANNOUNCEMENTS_BGCOLOR'			=> 'Couleur d’arrière-plan de l’annonce du forum',
-	'BOARD_ANNOUNCEMENTS_BGCOLOR_EXPLAIN'	=> 'Vous pouvez modifier la couleur d’arrière-plan de l’annonce en utilisant un code hexadécimal (ex.: FFFF80). Laissez ce champ vide pour utiliser la couleur par défaut.',
+	'BOARD_ANNOUNCEMENTS_BGCOLOR_EXPLAIN'	=> 'Vous pouvez modifier la couleur d’arrière-plan de l’annonce du forum en utilisant un code hexadécimal (ex.: FFFF80). Laissez ce champ vide pour utiliser la couleur par défaut.',
 
 	'BOARD_ANNOUNCEMENTS_TEXT'				=> 'Message de l’annonce du forum',
 	'BOARD_ANNOUNCEMENTS_PREVIEW'			=> 'Aperçu de l’annonce du forum',
