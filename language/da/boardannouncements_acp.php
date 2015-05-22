411777460e639af8964e70b43a56f8c2e2bc6f02
Matt Friedman <maf675@gmail.com>
2015-05-22 13:50:44 -0700

    Add Danish language

---
+++ language/da/boardannouncements_acp.php
@@ -0,0 +1,56 @@
+<?php
+/**
+*
+* Board Announcements extension for the phpBB Forum Software package.
+* Danish translation by jask (phpbb3.dk)
+*
+* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'BOARD_ANNOUNCEMENTS_SETTINGS'			=> 'Boardannonceringer',
+	'BOARD_ANNOUNCEMENTS_SETTINGS_EXPLAIN'	=> 'Her kan du oprette og ændre annonceringer som vises på alle sider på dit board.',
+
+	'BOARD_ANNOUNCEMENTS_ENABLE'			=> 'Aktiver boardannonceringer',
+	'BOARD_ANNOUNCEMENTS_GUESTS'			=> 'Tillad at gæster at kan læse boardannonceringer',
+	'BOARD_ANNOUNCEMENTS_DISMISS'			=> 'Tillad at brugere kan afvise denne boardannoncering',
+
+	'BOARD_ANNOUNCEMENTS_BGCOLOR'			=> 'Baggrundsfarve',
+	'BOARD_ANNOUNCEMENTS_BGCOLOR_EXPLAIN'	=> 'Du kan ændre boardannonceringens baggrundsfarve med en hexcode for den ønskede farve (f.eks: FFFF80). Efterlades feltet tomt anvendes standardfarven.',
+
+	'BOARD_ANNOUNCEMENTS_TEXT'				=> 'Annonceringens test',
+	'BOARD_ANNOUNCEMENTS_PREVIEW'			=> 'Vis annoncering',
+
+	'BOARD_ANNOUNCEMENTS_UPDATED'			=> 'Boardannoncering er opdateret.',
+));
