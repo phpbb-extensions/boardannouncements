6496e52aa8bf87989ad8b45f4c17d0422b0c88df
Matt Friedman <maf675@gmail.com>
2023-04-10 21:42:21 -0700

    Add ACP module tests
    Signed-off-by: Matt Friedman <maf675@gmail.com>

--- acp/board_announcements_module.php
+++ acp/board_announcements_module.php
@@ -1,12 +1,12 @@
 <?php
 /**
-*
-* Board Announcements extension for the phpBB Forum Software package.
-*
-* @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-*/
+ *
+ * Board Announcements extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2014 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
 namespace phpbb\boardannouncements\acp;
 
